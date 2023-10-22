607212f87900dbb73ad7b6f653076192f1492895
Nguyễn Minh Kha <nmkha19@clc.fitus.edu.vn>
2023-10-22 10:27:09 +0700

    fix combo counter UI

--- Assets/Scripts/UI/Effect/SineScale.cs
+++ Assets/Scripts/UI/Effect/SineScale.cs
@@ -6,8 +6,9 @@ public class SineScale : MonoBehaviour
 {
     [SerializeField] private float amplitude;
     [SerializeField] private float speed = 1;
+    
     private void Update(){
-        var sineValue = Mathf.Sin(Time.time*speed) * amplitude + 1;
+        var sineValue = Mathf.Sin(Time.time*speed) * amplitude + 1; // + 1 is offset the graph by 1 unit
         Vector3 scale = new Vector3(sineValue,sineValue,sineValue);
         transform.localScale = scale;
     }
--- Assets/Scripts/UI/TilesComboCounterUI.cs
+++ Assets/Scripts/UI/TilesComboCounterUI.cs
@@ -6,11 +6,13 @@ using UnityEngine.UI;
 
 public class TilesComboCounterUI : MonoBehaviour
 {
+    [SerializeField] private GameObject comboCanvas;
     [SerializeField] private TMP_Text comboMultiplierText;
     [SerializeField] private Slider durationSlider;
 
     public void UpdateMultiplierUI(int multiplier){
         comboMultiplierText.text = "x" + multiplier.ToString();
+        comboCanvas.SetActive(multiplier > 1);
     }
 
     public void UpdateDurationUI(float percentage){
